b90bb38eaa1835bd336edf044cf943c0e25a708c
dracuxan <nisargkhodke@gmail.com>
2025-08-27 22:10:20 +0530

    new: ip address finder

---
+++ ip_finder/main.c
@@ -0,0 +1,35 @@
+#include <ifaddrs.h>
+#include <netdb.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+
+int main() {
+  struct ifaddrs *addrs;
+  if (getifaddrs(&addrs) == -1) {
+    printf("getifaddrs() failed!\n");
+  }
+
+  struct ifaddrs *addr = addrs;
+
+  while (addr) {
+    int family = addr->ifa_addr->sa_family;
+    if (family == AF_INET || family == AF_INET6) {
+      printf("%s\t", addr->ifa_name);
+      printf("%s\t", family == AF_INET ? "IPv4" : "IPv6");
+
+      char ap[100];
+      const int family_size = family == AF_INET ? sizeof(struct sockaddr_in)
+                                                : sizeof(struct sockaddr_in6);
+      getnameinfo(addr->ifa_addr, family_size, ap, sizeof(ap), 0, 0,
+                  NI_NUMERICHOST);
+      printf("\t%s\n", ap);
+    }
+    addr = addr->ifa_next;
+  }
+
+  freeifaddrs(addrs);
+
+  return EXIT_SUCCESS;
+}
